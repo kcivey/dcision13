0e3b82e2f4e8423c996e9c0a4954a9b25f6db3b7
Keith C. Ivey <keith@iveys.org>
2013-04-27 23:02:36 -0400

    initial site version

---
+++ index.js
@@ -0,0 +1,84 @@
+jQuery(function ($) {
+
+var colors = {
+        Bonds: '#44aaaa',
+        Frumin: '#00aa00',
+        Mara: '#0000aa',
+        Silverman: '#aa0000'
+    },
+    map = L.map('map');
+L.tileLayer(
+    'http://{s}.tile.cloudmade.com/{key}/{style}/256/{z}/{x}/{y}.png',
+    {
+        key: '0c1c82bc050546ed93950f730c5a9366',
+        style: 998,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }
+).addTo(map);
+
+$.ajax({
+    url: 'dcision13.json',
+    dataType: 'json',
+    success: function (data) {
+        var layerOptions = {},
+            layer;
+        layerOptions.onEachFeature = function (feature, layer) {
+            layer.bindPopup(getPopupHtml(feature));
+        };
+        layerOptions.style = function (feature) {
+            var winner = getWinner(feature.properties.votes);
+            return {
+                fillColor: colors[winner],
+                fillOpacity: 0.6,
+                weight: 1,
+                color: 'white'
+            };
+        };
+        layer = L.geoJson(data, layerOptions).addTo(map);
+        map.fitBounds(layer.getBounds());
+    }
+});
+
+function getWinner(voteList) {
+    var winner;
+    $.each(voteList, function (candidate, votes) {
+        if (!winner || votes > voteList[winner]) {
+            winner = candidate;
+        }
+    });
+    return winner;
+}
+
+function getTotal(voteList) {
+    return _.reduce(_.values(voteList), function(memo, num){ return memo + num; }, 0);
+}
+
+function getPopupHtml(feature) {
+    var id = feature.id,
+        voteList = _.clone(feature.properties.votes),
+        winner = getWinner(voteList),
+        total = getTotal(voteList),
+        candidates = _.keys(voteList).sort(),
+        precinctNumber = id.replace('pct', ''),
+        html = '<h4>Precinct ' + precinctNumber +
+            ' (Ward ' + feature.properties.ward + ')</h4>';
+    html += '<table class="votes">';
+    candidates = _.without(candidates, 'Write-in');
+    candidates.push('Write-in', 'TOTAL');
+    voteList['TOTAL'] = total;
+    $.each(candidates, function (i, candidate) {
+        var votes = voteList[candidate],
+            percent = _.str.sprintf('%.1f', 100 * votes / total);
+        if (candidate == 'Write-in') {
+            candidate = '<i>' + candidate + '</i>';
+        }
+        html += (candidate == winner) ? '<tr class="winner">' : '<tr>';
+        html += '<td>' + candidate  + '</td><td class="right">' +
+            votes + '</td><td class="right">' + percent + '%</td></tr>';
+    });
+    html += '</table>';
+    return html;
+}
+
+});
